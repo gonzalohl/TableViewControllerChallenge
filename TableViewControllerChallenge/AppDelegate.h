a382e57feead478d54c3f9797d7faea70f392307
Gonzalo López <gonzalohl@Mac-mini-de-Gonzalo.local>
2015-11-04 16:31:12 -0300

    Initial Commit

---
+++ TableViewControllerChallenge/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TableViewControllerChallenge
+//
+//  Created by Gonzalo López on 4/11/15.
+//  Copyright (c) 2015 Gonzalo López. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
